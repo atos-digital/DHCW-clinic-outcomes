283fcc32098a6d012f9811508429f86f112cef33
Vivian Leong <vivianleong1124@gmail.com>
2024-01-30 13:41:32 +0000

    fix typo

--- ui/forms/outcomes/options/cancer_templ.go
+++ ui/forms/outcomes/options/cancer_templ.go
@@ -21,7 +21,7 @@ var (
 	NameCancerPathwayOther   = "cancer_pathway_other"
 )
 
-func Cacner(data models.CancerPathway) templ.Component {
+func Cancer(data models.CancerPathway) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
