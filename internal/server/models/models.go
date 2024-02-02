56d5205f45eaa9062583cac626a809fff67f158c
Vivian Leong <vivianleong1124@gmail.com>
2024-02-02 12:52:36 +0000

    rf: rename followup test fields

--- internal/server/models/models.go
+++ internal/server/models/models.go
@@ -45,8 +45,8 @@ type ClinicOutcomesFormPayload struct {
 
 	TestsRequiredBeforeFollowup string      `json:"tests_required_before_followup"`
 	TestsRequired               ArrayString `json:"tests_required"`
-	TestsUndertakenBy           ArrayString `json:"tests.undertaken"`
-	TestsBy                     ArrayString `json:"tests.by"`
+	TestsUndertakenBy           ArrayString `json:"tests_undertaken_by"`
+	TestsBy                     ArrayString `json:"tests_by"`
 	AddTest                     *string     `json:"add_test"`
 
 	OtherInformation string `json:"other_information"`
